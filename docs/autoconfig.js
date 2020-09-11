f81295f2fc113ccb6c38bbf89ea3d94e464022fd
mtwb47 <mtwb47@gmail.com>
2020-09-10 23:58:47 -0400

    term page

---
+++ docs/autoconfig.js
@@ -0,0 +1,4 @@
+// Tell ff to look for "firefox.cfg" at start up
+pref("general.config.filename", "firefox.cfg");
+pref("general.config.obscure_value", 0);
+pref("general.config.sandbox_enabled", false);
